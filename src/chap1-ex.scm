9b73ad47e06ee074bf666269ea4ce13b9502bfbd
cbpark <cbpark@gmail.com>
2015-02-17 01:00:06 +0900

    Finished up to 1.2.3

--- src/chap1-ex.scm
+++ src/chap1-ex.scm
@@ -1,18 +1,21 @@
 (load "chap1")
 
 ;;; Exercise 1.3
+
 (define (sum-of-squares-of-two-larger-numbers x y z)
   (cond ((and (<= x y) (<= x z)) (sum-of-squares y z))
-        ((<= y z)                (sum-of-squares x z))
-        (else                    (sum-of-squares x y))))
+        ((<= y z) (sum-of-squares x z))
+        (else (sum-of-squares x y))))
 
 ;;; Exercise 1.4
+
 (define (a-plus-abs-b a b)
   ((if (> b 0)
        +
        -) a b))
 
 ;;; Exercise 1.5
+
 (define (p)
   (p))
 
@@ -22,11 +25,13 @@
       y))
 
 ;;; Exercise 1.6
+
 (define (new-if predicate then-clause else-clause)
   (cond (predicate then-clause)
-        (else      else-clause)))
+        (else else-clause)))
 
 ;;; Exercise 1.7
+
 (define (sqrt-iter guess prev-guess x)
   (if (good-enough? guess prev-guess)
       guess
@@ -39,6 +44,7 @@
   (sqrt-iter 1.0 0.0 x))
 
 ;;; Exercise 1.8
+
 (define (cbrt-iter guess pre-guess x)
   (if (good-enough? guess pre-guess)
       guess
@@ -50,3 +56,58 @@
 
 (define (cbrt x)
   (cbrt-iter 1.0 0.0 x))
+
+;;; Exercise 1.10
+
+(define (A x y)
+  (cond ((= y 0) 0)
+        ((= x 0) (* 2 y))
+        ((= y 1) 2)
+        (else (A (- x 1) (A x (- y 1))))))
+
+(define (f n)
+  (A 0 n))
+
+(define (g n)
+  (A 1 n))
+
+(define (h n)
+  (A 2 n))
+
+(define (k n)
+  (* 5 n n))
+
+;;; Exercise 1.11
+
+(define (f-recur n)
+  (if (< n 3)
+      n
+      (+ (f-recur (- n 1)) (* 2 (f-recur (- n 2))) (* 3 (f-recur (- n 3))))))
+
+(define (f-iter n)
+  (define (iter a b c count)
+    (if (= count 0)
+        c
+        (iter (+ a (* 2 b) (* 3 c)) a b (- count 1))))
+  (iter 2 1 0 n))
+
+;;; Exercise 1.12
+
+(define (pascals-triangle row col)
+  (cond ((or (> col row) (< col 0)) 0)
+        ((= col 1) 1)
+        (else (+ (pascals-triangle (- row 1) (- col 1))
+                 (pascals-triangle (- row 1) col)))))
+
+;;; Exercise 1.15
+
+(define (cube x)
+  (* x x x))
+
+(define (p x)
+  (- (* 3 x) (* 4 (cube x))))
+
+(define (sine angle)
+  (if (not (> (abs angle) 0.1))
+      angle
+      (p (sine (/ angle 3.0)))))
--- src/chap1.scm
+++ src/chap1.scm
@@ -1,8 +1,10 @@
-;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; 1. Building Abstraction with Procedures
-;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;;;
 ;;; 1.1 The Elements of Programming
+;;;
 
 ;;; 1.1.1 Expressions
 
@@ -34,7 +36,7 @@
 
 (define (abs x)
   (cond ((< x 0) (- x))
-        (else    x)))
+        (else x)))
 
 (define (abs x)
   (if (< x 0)
@@ -78,3 +80,84 @@
   (+ x x))
 
 ;;; Local names
+
+;;; Internal definitions and block structure
+
+(define (sqrt x)
+  (define (good-enough? guess x)
+    (< (abs (- (square guess) x)) 0.001))
+  (define (improve guess x)
+    (average guess (/ x guess)))
+  (define (sqrt-iter guess x)
+    (if (good-enough? guess x)
+        guess
+        (sqrt-iter (improve guess x) x)))
+  (sqrt-iter 1.0 x))
+
+(define (sqrt x)
+  (define (good-enough? guess)
+    (< (abs (- (square guess) x)) 0.001))
+  (define (improve guess)
+    (average guess (/ x guess)))
+  (define (sqrt-iter guess)
+    (if (good-enough? guess)
+        guess
+        (sqrt-iter (improve guess))))
+  (sqrt-iter 1.0))
+
+;;;
+;;; 1.2 Procedures and the Processes They Generate
+;;;
+
+;;; 1.2.1 Linear Recursion and Iteration
+
+(define (factorial n)
+  (if (= n 1)
+      1
+      (* n (factorial (- n 1)))))
+
+(define (factorial n)
+  (fact-iter 1 1 n))
+
+(define (fact-iter product counter max-count)
+  (if (> counter max-count)
+      product
+      (fact-iter (* counter product)
+                 (+ counter 1)
+                 max-count)))
+
+;;; 1.2.2 Tree Recursion
+
+(define (fib n)
+  (cond ((= n 0) 0)
+        ((= n 1) 1)
+        (else (+ (fib (- n 1)) (fib (- n 2))))))
+
+(define (fib n)
+  (fib-iter 1 0 n))
+
+(define (fib-iter a b count)
+  (if (= count 0)
+      b
+      (fib-iter (+ a b) a (- count 1))))
+
+;;; Example: Counting change
+
+(define (count-change amount)
+  (cc amount 5))
+
+(define (cc amount kinds-of-coins)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (= kinds-of-coins 0)) 0)
+        (else (+ (cc amount (- kinds-of-coins 1))
+                 (cc (- amount (first-denomination kinds-of-coins))
+                     kinds-of-coins)))))
+
+(define (first-denomination kinds-of-coins)
+  (cond ((= kinds-of-coins 1) 1)
+        ((= kinds-of-coins 2) 5)
+        ((= kinds-of-coins 3) 10)
+        ((= kinds-of-coins 4) 25)
+        ((= kinds-of-coins 5) 50)))
+
+;;; 1.2.3 Orders of Growth
