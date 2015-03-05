2ba4590adc9a33f45e1aaa2aa7236c1a16cc671d
cbpark <cbpark@gmail.com>
2015-03-06 00:10:49 +0900

    Update for 2.2.3

--- src/chap2-ex.scm
+++ src/chap2-ex.scm
@@ -367,3 +367,27 @@
         (append rest (map (lambda (x)
                             (cons (car s)
                                   x)) rest)))))
+
+;;; Exercise 2.33
+
+(define (map p sequence)
+  (accumulate (lambda (x y) (cons (p x) y)) '() sequence))
+
+(define (append seq1 seq2)
+  (accumulate cons seq2 seq1))
+
+(define (length sequence)
+  (accumulate (lambda (x y) (+ 1 y)) 0 sequence))
+
+;;; Exercise 2.34
+
+(define (horner-eval x coefficient-sequence)
+  (accumulate (lambda (this-coeff higher-terms)
+                (+ (* higher-terms x) this-coeff)) 0 coefficient-sequence))
+
+;;; Exercise 2.35
+
+(define (count-leaves t)
+  (accumulate + 0 (map (lambda (n) (if (pair? n)
+                                       (count-leaves n)
+                                       1)) t)))
