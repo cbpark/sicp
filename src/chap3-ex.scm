0b963e3a9e832723703fc123f2d486cb6f494ae1
cbpark <cbpark@gmail.com>
2015-03-19 00:13:09 +0100

    Finished up to 3.3.2

--- src/chap3-ex.scm
+++ src/chap3-ex.scm
@@ -122,3 +122,189 @@
 
 (define (make-joint account old-password new-password)
   ((account old-password 'make-joint) new-password))
+
+;;; Exercise 3.12
+
+(define (append! x y)
+  (set-cdr! (last-pair x) y)
+  x)
+
+(define (last-pair x)
+  (if (null? (cdr x)) x (last-pair (cdr x))))
+
+;;; Exercise 3.13
+
+(define (make-cycle x)
+  (set-cdr! (last-pair x) x)
+  x)
+
+;;; Exercise 3.14
+
+(define (mystery x)
+  (define (loop x y)
+    (if (null? x)
+        y
+        (let ((temp (cdr x)))
+          (set-cdr! x y)
+          (loop temp x))))
+  (loop x '()))
+
+;;; Exercise 3.16
+
+(define (count-pairs x)
+  (if (not (pair? x))
+      0
+      (+ (count-pairs (car x))
+         (count-pairs (cdr x))
+         1)))
+
+;;; Exercise 3.17
+
+(define (count-pairs x)
+  (let ((counted '()))
+    (define (counter x)
+      (if (or (not (pair? x)) (memq x counted))
+          0
+          (begin
+            (set! counted (cons x counted))
+            (+ (counter (car x)) (counter (cdr x)) 1))))
+    (counter x)))
+
+;;; Exercise 3.18
+
+(define (cycle? x)
+  (let ((visited '()))
+    (define (iter x)
+      (set! visited (cons x visited))
+      (cond ((null? (cdr x)) false)
+            ((memq (cdr x) visited) true)
+            (else (iter (cdr x)))))
+    (iter x)))
+
+;;; Exercise 3.19
+
+(define (cycle? x)
+  (define (iter a b)
+    (cond ((not (pair? a)) false)
+          ((not (pair? b)) false)
+          ((eq? a b) true)
+          ((eq? a (cdr b)) true)
+          (else (iter (cdr a) (cddr b)))))
+  (iter (cdr x) (cddr x)))
+
+;;; Exercise 3.21
+
+(define (print-queue queue)
+  (display (car queue)))
+
+;;; Exercise 3.22
+
+(define (make-queue)
+  (let ((front-ptr '())
+        (rear-ptr '()))
+    (define (empty-queue?) (null? front-ptr))
+    (define (set-front-ptr! item) (set! front-ptr item))
+    (define (set-rear-ptr! item) (set! rear-ptr item))
+    (define (front-queue)
+      (if (empty-queue?)
+          (error "FRONT called with an empty queue")
+          (car front-ptr)))
+    (define (insert-queue! item)
+      (let ((new-pair (cons item '())))
+        (cond ((empty-queue? queue)
+               (set-front-ptr! queue new-pair)
+               (set-rear-ptr! queue new-pair)
+               queue)
+              (else (set-cdr! (rear-ptr queue) new-pair)
+                    (set-rear-ptr! queue new-pair)
+                    queue))))
+    (define (delete-queue!)
+      (cond ((empty-queue? queue)
+             (error "DELETE! called with an empty queue" queue))
+            (else (set-front-ptr! queue (cdr (front-ptr queue)))
+                  queue)))
+    (define (dispatch m)
+      (cond ((eq? m 'empty-queue?) empty-queue?)
+            ((eq? m 'front-queue) front-queue)
+            ((eq? m 'insert-queue!) insert-queue!)
+            ((eq? m 'delete-queue!) delete-queue!)
+            (else (error "QUEUE: undefined operation" m))))
+    dispatch))
+
+;;; Exercise 3.23
+
+(define (front-ptr deque) (car deque))
+(define (rear-ptr deque) (cdr deque))
+(define (set-front-ptr! deque item) (set-car! deque item))
+(define (set-rear-ptr! deque item) (set-cdr! deque item))
+
+(define (empty-deque? deque)
+  (and (null? (front-ptr deque)) (null? (rear-ptr deque))))
+
+(define (make-deque) (cons '() '()))
+
+(define (front-insert-deque! deque item)
+  (let ((new-pair (cons item (cons '() '()))))
+    (cond ((empty-deque? deque)
+           (set-front-ptr! deque new-pair)
+           (set-rear-ptr! deque new-pair)
+           '())
+          (else
+           (set-cdr! (cdr new-pair) (front-ptr deque))
+           (set-car! (cdr (front-ptr deque)) new-pair)
+           (set-front-ptr! deque new-pair)
+           '()))))
+
+(define (rear-insert-deque! deque item)
+  (let ((new-pair (cons item (cons '() '()))))
+    (cond ((empty-deque? deque)
+           (set-front-ptr! deque new-pair)
+           (set-rear-ptr! deque new-pair)
+           '())
+          (else
+           (set-car! (cdr new-pair) (rear-ptr deque))
+           (set-cdr! (cdr (rear-ptr deque)) new-pair)
+           (set-rear-ptr! deque new-pair)
+           '()))))
+
+(define (front-deque deque)
+  (if (empty-deque? deque)
+      (error "FRONT called with an empty deque" deque)
+      (car (front-ptr deque))))
+
+(define (rear-deque deque)
+  (if (empty-deque? deque)
+      (error "REAR called with an empty deque" deque)
+      (car (rear-ptr deque))))
+
+(define (front-delete-deque! deque)
+  (cond ((empty-deque? deque)
+         (error "FRONT-DELETE! called with an empty deque" deque))
+        ((eq? (front-ptr deque) (rear-ptr deque))
+         (set-front-ptr! deque '())
+         (set-rear-ptr! deque '())
+         '())
+        (else
+         (set-front-ptr! deque (cddr (front-ptr deque)))
+         (set-car! (cdr (front-ptr deque)) '())
+         '())))
+
+(define (rear-delete-deque! deque)
+  (cond ((empty-deque? deque)
+         (error "REAR-DELETE! called with an empty deque" deque))
+        ((eq? (front-ptr deque) (rear-ptr deque))
+         (set-front-ptr! deque '())
+         (set-rear-ptr! deque '())
+         '())
+        (else
+         (set-rear-ptr! deque (cadr (rear-ptr deque)))
+         (set-cdr! (cdr (rear-ptr deque)) '())
+         '())))
+
+(define (print-deque deque)
+  (define (print q)
+    (if (null? q)
+        '()
+        (cons (car q)
+              (print (cddr q)))))
+  (display (print (front-ptr deque))))
--- src/chap3.scm
+++ src/chap3.scm
@@ -120,3 +120,79 @@
 ;;;
 ;;; 3.2 The Environment Model of Evaluation
 ;;;
+
+;;; 3.2.1 The Rules for Evaluation
+
+;;; 3.2.2 Applying Simple Procedures
+
+;;; 3.2.3 Frames as the Repository of Local State
+
+;;; 3.2.4 Internal Definitions
+
+;;;
+;;; 3.3 Modeling with Mutable Data
+;;;
+
+;;; 3.3.1 Mutable List Structure
+
+;; (define (cons x y)
+;;   (let ((new (get-new-pair)))
+;;     (set-car! new x)
+;;     (set-cdr! new y)
+;;     new))
+
+;;; Sharing and identity
+
+;;; Mutation is just assignment
+
+;; (define (cons x y)
+;;   (define (set-x! v) (set! x v))
+;;   (define (set-y! v) (set! y v))
+;;   (define (dispatch m)
+;;     (cond ((eq? m 'car) x)
+;;           ((eq? m 'cdr) y)
+;;           ((eq? m 'set-car!) set-x!)
+;;           ((eq? m 'set-cdr!) set-y!)
+;;           (else (error "Undefined operation: CONS" m))))
+;;   dispatch)
+;; (define (car z) (z 'car))
+;; (define (cdr z) (z 'cdr))
+;; (define (set-car! z new-value)
+;;   ((z 'set-car!) new-value) z)
+;; (define (set-cdr! z new-value)
+;;   ((z 'set-cdr!) new-value) z)
+
+;;; 3.3.2 Representing Queues
+
+(define (front-ptr queue) (car queue))
+(define (rear-ptr queue) (cdr queue))
+(define (set-front-ptr! queue item)
+  (set-car! queue item))
+(define (set-rear-ptr! queue item)
+  (set-cdr! queue item))
+
+(define (empty-queue? queue)
+  (null? (front-ptr queue)))
+
+(define (make-queue) (cons '() '()))
+
+(define (front-queue queue)
+  (if (empty-queue? queue)
+      (error "FRONT called with an empty queue" queue)
+      (car (front-ptr queue))))
+
+(define (insert-queue! queue item)
+  (let ((new-pair (cons item '())))
+    (cond ((empty-queue? queue)
+           (set-front-ptr! queue new-pair)
+           (set-rear-ptr! queue new-pair)
+           queue)
+          (else (set-cdr! (rear-ptr queue) new-pair)
+                (set-rear-ptr! queue new-pair)
+                queue))))
+
+(define (delete-queue! queue)
+  (cond ((empty-queue? queue)
+         (error "DELETE! called with an empty queue" queue))
+        (else (set-front-ptr! queue (cdr (front-ptr queue)))
+              queue)))
