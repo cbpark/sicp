82410ec7e6c2b9ad3157b63425e18f28c68e48d3
cbpark <cbpark@gmail.com>
2015-03-23 00:28:39 +0100

    Finished up to 3.4.2

--- src/chap3.scm
+++ src/chap3.scm
@@ -89,7 +89,7 @@
 (define (make-simplified-withdraw balance)
   (lambda (amount)
     (set! balance (- balance amount))
-    balace))
+    balance))
 
 (define (make-decrementer balance)
   (lambda (amount)
@@ -625,4 +625,112 @@
 
 ;;;
 ;;; 3.4 Concurrency: Time Is of the Essence
+;;;
+
+;;; 3.4.1 The Nature of Time in Concurrent Systems
+
+;;; Correct behavior of concurrent programs
+
+;;; 3.4.2 Mechanisms for Controlling Concurrency
+
+;;; Serializing access to shared state
+
+;;; Serializer in Scheme
+
+(define (make-account balance)
+  (define (withdraw amount)
+    (if (>= balance amount)
+        (begin (set! balance (- balance amount))
+               balance)
+        "Insufficient funds"))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  (let ((protected (make-serializer)))
+    (define (dispatch m)
+      (cond ((eq? m 'withdraw) (protected withdraw))
+            ((eq? m 'deposit) (protected deposit))
+            ((eq? m 'balance) balance)
+            (else (error "Unknown request: MAKE-ACCOUNT" m))))
+    dispatch))
+
+;;; Complexity of using multiple shared resources
+
+(define (exchange account1 account2)
+  (let ((difference (- (account1 'balance)
+                       (account2 'balance))))
+    ((account1 'withdraw) difference)
+    ((account2 'deposit) difference)))
+
+(define (make-account-and-serializer balance)
+  (define (withdraw amount)
+    (if (>= balance amount)
+        (begin (set! balance (- balance amount))
+               balance)
+        "Insufficient funds"))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  (let ((balance-serializer (make-serializer)))
+    (define (dispatch m)
+      (cond ((eq? m 'withdraw) (protected withdraw))
+            ((eq? m 'deposit) (protected deposit))
+            ((eq? m 'balance) balance)
+            ((eq? m 'serializer) balance-serializer)
+            (else (error "Unknown request: MAKE-ACCOUNT" m))))
+    dispatch))
+
+(define (deposit account amount)
+  (let ((s (account 'serializer))
+        (d (account 'deposit)))
+    ((s d) amount)))
+
+(define (serialized-exchange account1 account2)
+  (let ((serializer1 (account1 'serializer))
+        (serializer2 (account2 'serializer)))
+    ((serializer1 (serializer2 exchange))
+     account1
+     account2)))
+
+;;; Implementing serializers
+
+(define (make-serializer)
+  (let ((mutex (make-mutex)))
+    (lambda (p)
+      (define (serialized-p . args)
+        (mutex 'acquire)
+        (let ((val (apply p args)))
+          (mutex 'release)
+          val))
+      serialized-p)))
+
+(define (make-mutex)
+  (let ((cell (list false)))
+    (define (the-mutex m)
+      (cond ((eq? m 'acquire) (if (test-and-set! cell)
+                                  (the-mutex 'acquire)))  ; retry
+            ((eq? m 'release) (clear! cell))))
+    the-mutex))
+(define (clear! cell) (set-car! cell false))
+
+;; (define (test-and-set! cell)
+;;   (if (car cell)
+;;       true
+;;       (begin (set-car! cell true)
+;;              false)))
+
+(define (test-and-set! cell)
+  (without-interrupts
+   (lambda ()
+     (if (car cell)
+         true
+         (begin (set-car! cell true)
+                false)))))
+
+;;; Deadlock
+
+;;; Concurrency, time, and communication
+
+;;;
+;;; 3.5 Streams
 ;;;
